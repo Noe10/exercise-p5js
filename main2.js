8a2af145d0c6388a87bd983371fff48900f0dc5c
noe10 <escorpio.rosa.94@gmail.com>
2020-02-06 00:00:36 -0500

    se mueve y se queda quieta

--- main2.js
+++ main2.js
@@ -4,8 +4,10 @@ const canvasHeight = 1000;
 let gridSize = 25;
 
 let cellSize = canvasWidth / gridSize;
-let moveX = 4;
+let moveX = 8;
 let moveY = 0;
+let p2x = 8;
+let p2y = 3;
 let fr =1 ;
 
 function cell(pX, pY) {
@@ -17,9 +19,16 @@ function cell(pX, pY) {
   rect(pX, pY, cellSize, cellSize);
 
 }
+function cell2(p2X, p2Y){
+  p2X = cellSize * p2X;
+  p2Y = cellSize * p2Y;
+   
+ 
+  rect(p2X, p2Y, cellSize, cellSize);
+}
 
 function setup() {
-  
+
 
   createCanvas(canvasWidth, canvasHeight);
   frameRate(fr);
@@ -28,13 +37,19 @@ function setup() {
 function draw() {
   background(0);
  
+ cell2(p2x,p2y);
+
   moveY++;
  
   frameRate(fr)
   
   cell(moveX, moveY);
-  if(moveY == gridSize ){
-    moveY = 0;
+
+  if(moveY == p2y-1 && moveX == p2x){
+    moveY--;
+  }
+  if(moveY == gridSize){
+    moveY=0;
   }
   
 }
